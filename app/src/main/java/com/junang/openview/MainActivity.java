0507d055372445e735d97b8d2eadc316b73ed3aa
junius.ang <junius.ang@gmail.com>
2016-08-28 05:20:14 +0700

    added drag animation when touched

--- app/src/main/java/com/junang/openview/MainActivity.java
+++ app/src/main/java/com/junang/openview/MainActivity.java
@@ -2,14 +2,21 @@ package com.junang.openview;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ViewFlipper;
+
+import com.junang.openview.util.ViewUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper vFlipper;
+    boolean isFirst = false;
+    View v1, v2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,54 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                if(isFirst) {
+                    vFlipper.showNext();
+                }
+                else{
+                    vFlipper.showPrevious();
+                }
+                isFirst = !isFirst;
             }
         });
+        vFlipper = (ViewFlipper) findViewById(R.id.view_flipper);
+        v1 = ViewUtil.findById(this, R.id.view1);
+        v2 = ViewUtil.findById(this, R.id.view2);
+        v1.setOnTouchListener(onTouchListener);
+        v2.setOnTouchListener(onTouchListener);
+    }
+
+    View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+
+            switch (event.getAction()) {
+
+                case MotionEvent.ACTION_DOWN:
+
+                    dX = view.getX() - event.getRawX();
+                    dY = view.getY() - event.getRawY();
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+
+                    view.animate()
+                            .x(event.getRawX() + dX)
+                            .y(event.getRawY() + dY)
+                            .setDuration(0)
+                            .start();
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+    };
+    float dX, dY;
+
+    public void onClick(View v){
+
     }
 
     @Override
