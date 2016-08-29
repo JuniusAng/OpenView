735741794b4972817533d26577381a14336f4763
junius.ang <junius@traveloka.com>
2016-08-29 15:46:58 +0700

    added Status bar change method

--- app/src/main/java/com/junang/openview/delegation/CoreAppBarDelegate.java
+++ app/src/main/java/com/junang/openview/delegation/CoreAppBarDelegate.java
@@ -1,8 +1,12 @@
 package com.junang.openview.delegation;
 
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.junang.openview.R;
 import com.junang.openview.util.ViewUtil;
@@ -51,6 +55,22 @@ public class CoreAppBarDelegate<T extends CoreDelegateDependency> extends CoreDe
         appCompatActivity.setSupportActionBar(toolbarDelegate.getToolbar());
     }
 
+    /**
+     * set custom Status bar color rather than predefined primaryDark
+     * @param appCompatActivity
+     * @param color
+     */
+    public void setStatusBarColor(AppCompatActivity appCompatActivity, int color){
+        if(Build.VERSION.SDK_INT >= 21){
+            Window window = appCompatActivity.getWindow();
+            // clear FLAG_TRANSLUCENT_STATUS flag:
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            // finally change the color
+            window.setStatusBarColor(ContextCompat.getColor(appCompatActivity, color));
+        }
+    }
 
     public CollapsingToolbarLayout getCollapsingToolbarLayout() {
         return vCollapsingToolbarLayout;
